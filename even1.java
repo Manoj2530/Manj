659a5f44c7ad9a9d48810ac35ea92145b719732f
Manoj2530 <gmanojkumar009@gmail.com>
2021-12-07 22:36:50 +0530

    even code

---
+++ even1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class even
+{
+public static void main(String m[])
+{
+  int even;
+ even=Integer.parseInt(m[0]);
+ if(even%2==0)
+ System.out.println("Even");
+ else
+ System.out.println("odd");
+}
+}
